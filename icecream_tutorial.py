c2caf9d0303fd01633520a1f43fdfeefaa51e692
Jeff Watts <5voltsgc@gmail.com>
2021-08-27 12:03:04 -0600

    continued tutorial

---
+++ icecream_tutorial.py
@@ -0,0 +1,16 @@
+from icecream import ic
+
+def square_of(num):
+    return num * num
+
+print(square_of(2))
+print(square_of(3))
+print(square_of(4))
+
+print('square of 2:', square_of(2))
+print('square of 3:', square_of(3))
+print('square of 4:', square_of(4))
+
+ic(square_of(2))
+ic(square_of(3))
+ic(square_of(4))
