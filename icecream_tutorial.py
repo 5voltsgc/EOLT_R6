810ed9070db885c02b58d26df5de57a53c4ca03c
Jeff Watts <5voltsgc@gmail.com>
2021-08-27 12:16:32 -0600

    completed tutorial

--- icecream_tutorial.py
+++ icecream_tutorial.py
@@ -1,4 +1,19 @@
+# https://towardsdatascience.com/do-not-use-print-for-debugging-in-python-anymore-6767b6f1866d
 from icecream import ic
+from icecream.icecream import colorizedStderrPrint
+from datetime import datetime
+# ic.disable()
+
+def now():
+    return f'[{datetime.now()}] '
+
+ic.configureOutput(prefix=now)
+# ic('test')
+
+my_dict = {
+    'name': 'Chris',
+    'age': 33
+}
 
 def square_of(num):
     return num * num
@@ -14,3 +29,40 @@ print('square of 4:', square_of(4))
 ic(square_of(2))
 ic(square_of(3))
 ic(square_of(4))
+
+ic(my_dict['name'])
+
+class Dog():
+    num_legs = 4
+    tail = True
+    nose = "cold"
+
+dog = Dog()
+
+ic(dog.num_legs)
+ic(dog.tail)
+ic(dog.nose)
+print(dog.tail)
+
+input = "Chris"
+
+if input =="Chris":
+    ic()
+else:
+    ic()
+
+def check_user(username):
+    if username == 'Chris':
+        # do something
+        ic()
+    else:
+        # do something else
+        ic()
+check_user('Chris')
+check_user('Jade')
+
+num = 2
+square_of_num = square_of(ic(num))
+
+if ic(square_of_num) == pow(num, 2):
+    ic('Correct!')
